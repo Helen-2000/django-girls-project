f0b639a80ca5571bd48bb6fe0e796b7b4bfc5773
Helen Salazar <helen2000@ciencias.unam.mx>
2019-07-27 17:42:46 -0500

    Django girls blog

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,18 @@
 """MyBlog views"""
 
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'myblog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'myblog/post_list.html', {'posts' : posts})
+
+def welcome(request):
+    return render(request, 'myblog/welcome.html')
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'myblog/post_detail.html', {'post': post})
+
+
